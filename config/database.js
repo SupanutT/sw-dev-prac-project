ab1ccb2ec56b20d66dc8f7905d31c9412fd8eadc
SupanutT <supanut.tangsin@gmail.com>
2024-04-15 14:19:03 +0700

    fix : change to use env var in database connection

--- config/database.js
+++ config/database.js
@@ -3,24 +3,24 @@ dotenv.config({ path: './config/config.env' });
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
+        username: process.env.DEV_DB_USER,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DB_DATABASE,
+        host: process.env.DEV_DB_HOST,
         dialect: 'mysql'
     },
     test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
+        username: process.env.TEST_DB_USER,
+        password: process.env.TEST_DB_PASSWORD,
+        database: process.env.TEST_DB_DATABASE,
+        host: process.env.TEST_DB_HOST,
         dialect: 'mysql'
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
+        username: process.env.PROD_DB_USER,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_DATABASE,
+        host: process.env.PROD_DB_HOST,
         dialect: 'mysql'
     }
 };
